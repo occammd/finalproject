8b838340979ceae4c090808dcc61e07165f740e8
OccamMDCEO <gilesfc@me.com>
2013-03-07 16:09:04 -0500

    last

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,26 @@
 
 ActiveRecord::Schema.define(:version => 20130305140259) do
 
+  create_table "cases", :force => true do |t|
+    t.date     "operation_date"
+    t.integer  "position"
+    t.integer  "patient_id"
+    t.integer  "surgeon_id"
+    t.integer  "anesthesia_id"
+    t.string   "surgery"
+    t.string   "triage"
+    t.string   "case_cart"
+    t.string   "or_room"
+    t.integer  "time_estimate"
+    t.text     "notes"
+    t.boolean  "finished"
+    t.boolean  "started"
+    t.date     "started_at"
+    t.date     "finished_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
   create_table "patients", :force => true do |t|
     t.string   "mrn"
     t.string   "name"
